a899c4cd92395d8bc760924256c846264df3fc19
Dave Calnan <d@ve.ie>
2018-09-14 18:11:14 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>3xW</title>
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+</head>
+<body class="antialiased w-screen h-screen flex flex-col border-blue-lightest border-16 bg-grey-light text-grey-darkest">
+    <main class="pt-16 xs:pt-24 sm:pt-32 px-4 max-w-md mx-auto flex-1">
+        <h1 class="font-serif text-semibold mb-2">3xW</h1>
+        <p class="mb-8">is a collection of companies working to help small businesses compete.</p>
+        <p class="mb-8"><a href="https://sourceacademy.co">SourceAcademy</a> makes affordable brochure- and content-driven websites and works with top student talent.</p>
+        <p class="mb-8">3xW Digital makes bespoke, well-designed, converting e-commerce websites for independent makers and retailers.</p>
+        <p><a href="https://clicksandmortar.ie">Clicks &amp; Mortar</a> provides retailers with resources and tools to grow and compete online.</p>
+    </main>
+    <footer class="xs:text-center m-4">
+        <small>&copy; 3xW Digital Limited, 2018</small>
+    </footer>
+</body>
+</html>
